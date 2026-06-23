1ec1f6312dbc93d44eb4213b96c311167211533f
Jamesokooboh <okoobohjames@gmail.com>
2026-06-23 13:45:51 +0100

    added new files

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector(".menu-btn")
+.addEventListener("click",()=>{
+
+const nav=document.querySelector(".nav-links");
+
+if(nav.style.display==="flex"){
+    nav.style.display="none";
+}
+else{
+    nav.style.display="flex";
+}
+
+});
